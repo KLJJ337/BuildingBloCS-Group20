02440b8ef9b05ab4583e63202898785629881331
KLJJ337 <2697341029k@gmail.com>
2023-09-07 14:42:04 +0800

    b

--- src/Announcement.js
+++ src/Announcement.js
@@ -4,22 +4,41 @@ import './Announcement.css';
 
 function Announcement() {
     return (
-        
-        <div className="announcementDiv">
-            <h2>Announcement</h2>
-            <div className="announcementThreads">
-                <div className="announcement">
-                    <h3>Title</h3>
-                    <p>Content</p>
+        <div className="second">
+            <div className="announcementDiv">
+                <h2>Announcement</h2>
+                <div className="announcementThreads">
+                    <div className="announcement">
+                        <h3>Title</h3>
+                        <p>Content</p>
+                    </div>
+                    <div className="announcement">
+                        <h3>Title</h3>
+                        <p>Content</p>
+                    </div>
+                    <div className="announcement">
+                        <h3>Title</h3>
+                        <p>Content</p>
+                    </div>
                 </div>
-                <div className="announcement">
-                    <h3>Title</h3>
-                    <p>Content</p>
+            </div>
+            <div className="storyCorner">
+                <h2>Story Corner</h2>
+                <div className="storyThreads">
+                    <div className="story">
+                        <h3>Title</h3>
+                        <p>Content</p>
+                    </div>
+                    <div className="story">
+                        <h3>Title</h3>
+                        <p>Content</p>
+                    </div>
+                    <div className="story">
+                        <h3>Title</h3>
+                        <p>Content</p>
+                    </div>
                 </div>
-                <div className="announcement">
-                    <h3>Title</h3>
-                    <p>Content</p>
-                </div>
+
             </div>
         </div>
       );
